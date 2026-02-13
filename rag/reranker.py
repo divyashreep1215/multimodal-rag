9410b3fdde780317f73c6ac8c68a0a6237752521
Divyashree P <divyashreep2004@gmail.com>
2026-02-13 10:40:22 +0530

    Create reranker.py

---
+++ rag/reranker.py
@@ -0,0 +1,14 @@
+import numpy as np
+from rag.embeddings import get_text_embedding
+
+def rerank(query, documents):
+    query_vec = get_text_embedding(query)
+    scores = []
+
+    for doc in documents:
+        doc_vec = get_text_embedding(doc)
+        score = np.dot(query_vec, doc_vec)
+        scores.append((doc, score))
+
+    scores.sort(key=lambda x: x[1], reverse=True)
+    return [doc for doc, _ in scores]
